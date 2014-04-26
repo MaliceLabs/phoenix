e9a9438feae1b44094a2a5462d34c4b21b6657c8
Charmander <campersander@gmail.com>
2014-04-25 17:37:44 -0700

    Terms of service, submission agreement, and AUP.

--- routes/general.js
+++ routes/general.js
@@ -8,4 +8,8 @@ var router = new express.Router();
 
 router.get('/', promiseResponse.html(general.home));
 
+router.get('/support/terms', promiseResponse.html(general.terms));
+router.get('/support/submission-agreement', promiseResponse.html(general.submissionAgreement));
+router.get('/support/acceptable-upload-policy', promiseResponse.html(general.aup));
+
 module.exports = router;
